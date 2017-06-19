d3afc215e2437d97565ac33d00451b432560039f
Stephan Foster <sxfoster@godaddy.com>
2017-06-18 17:45:41 -0700

    Completed Problem 2

---
+++ CST-105_assignments/src/Problem2.java
@@ -0,0 +1,8 @@
+
+public class Problem2 {
+	public static void main(String[] args) {
+		System.out.println("My name is Stephan.");
+		System.out.println("My favorite color is Black");
+		System.out.println("If I won the lottery, I would retire and give to all my family and friends.");
+	}
+}
